732904afeb1d57630f7d724311a3daa56e0d265f
Mikael Lind <elemel@elemel.se>
2009-02-01 23:00:13 +0100

    switched from Pygame to Pyglet (no collisions or shots at the moment)

--- lib/void/main.py
+++ lib/void/main.py
@@ -21,17 +21,14 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 # OTHER DEALINGS IN THE SOFTWARE.
 
-from __future__ import division
-import pygame, sys, random, math, numpy
-from pygame.locals import *
-from OpenGL.GL import *
+import sys, random, math, numpy, pyglet
+from pyglet.gl import *
 
-class Body(pygame.sprite.Sprite):
+class Body(object):
     radius = 1.0
     max_rotation_speed = 10.0
 
     def __init__(self, groups, created_at):
-        pygame.sprite.Sprite.__init__(self, *groups)
         self.pos = numpy.array([0.0, 0.0])
         self.created_at = created_at
         self.velocity = numpy.array([0.0, 0.0])
@@ -41,11 +38,6 @@ class Body(pygame.sprite.Sprite):
     def update(self, delta_time):
         self.pos += self.velocity * delta_time
         self.rotation += self.rotation_speed * delta_time
-
-    @property
-    def rect(self):
-        return pygame.Rect(self.pos[0] - self.radius, self.pos[1] - self.radius,
-                           self.radius * 2.0, self.radius * 2.0)
 
     def draw(self):
         x, y = self.pos
@@ -85,15 +77,7 @@ class Ship(Body):
                                          math.sin(self.rotation)]))
 
     def update_cannon(self, delta_time):
-        if (self.firing and
-            self.fired_at + self.cooldown < pygame.time.get_ticks() / 1000.0):
-            direction = numpy.array([math.cos(self.rotation),
-                                     math.sin(self.rotation)])
-            shot = self.create_shot()
-            shot.pos = self.pos + direction * self.gun_pos
-            shot.rotation = self.rotation
-            shot.velocity = self.velocity + self.shot_velocity * direction
-            self.fired_at = pygame.time.get_ticks() / 1000.0
+        pass
 
     def draw_geometry(self):
         glBegin(GL_TRIANGLES)
@@ -150,23 +134,63 @@ class Plasma(Body):
         glVertex2d(0.1, -0.1)
         glEnd()
 
-class Game(object):
+class VoidWindow(pyglet.window.Window):
     def __init__(self):
-        self.bodies = pygame.sprite.Group()
-        self.player_group = pygame.sprite.Group()
-        self.asteroids = pygame.sprite.Group()
-        self.shots = pygame.sprite.Group()
+        pyglet.window.Window.__init__(self, width=640, height=480,
+                                      caption="Void")
+        pyglet.clock.schedule_interval(self.update, 0.01)
         def create_shot():
-            shot = Plasma([self.shots, self.bodies], self.time)
+            shot = Plasma([], 0.0)
             return shot
-        self.player_ship = Ship([self.player_group, self.bodies],
-                                self.time, create_shot)
+        self.player_ship = Ship([], 0.0, create_shot)
+        self.asteroids = []
         for _ in xrange(10):
-            self.create_asteroid()
+            self.asteroids.append(self.generate_asteroid())
+
+    def update(self, dt):
+        self.player_ship.update(dt)
+        for asteroid in self.asteroids:
+            asteroid.update(dt)
+        self.apply_player_ship_constraints()
         
-    def create_asteroid(self):
-        asteroid = Asteroid.generate([self.asteroids, self.bodies], self.time)
-        angle = (random.random() - 0.5) * math.pi
+    def on_draw(self):
+        glClearColor(0.0, 0.0, 0.0, 0.0)
+        self.clear()
+        glPushMatrix()
+        glTranslated(self.width / 2.0, self.height / 2.0, 0.0)
+        glScaled(20.0, 20.0, 20.0)
+        x, y = self.player_ship.pos
+        glTranslated(-x, -y, 0.0)
+        self.player_ship.draw()
+        for asteroid in self.asteroids:
+            asteroid.draw()
+        glPopMatrix()
+
+    def on_key_press(self, symbol, modifiers):
+        if symbol == pyglet.window.key.ESCAPE:
+            sys,exit()
+        if symbol == pyglet.window.key.UP:
+            self.player_ship.thrusting = True
+        if symbol == pyglet.window.key.SPACE:
+            self.player_ship.firing = True
+        if symbol == pyglet.window.key.LEFT:
+            self.player_ship.rotation_speed = \
+                self.player_ship.max_rotation_speed
+        if symbol == pyglet.window.key.RIGHT:
+            self.player_ship.rotation_speed = \
+                -self.player_ship.max_rotation_speed
+
+    def on_key_release(self, symbol, modifiers):
+        if symbol == pyglet.window.key.UP:
+            self.player_ship.thrusting = False
+        if symbol == pyglet.window.key.SPACE:
+            self.player_ship.firing = False
+        if symbol in (pyglet.window.key.LEFT, pyglet.window.key.RIGHT):
+            self.player_ship.rotation_speed = 0.0
+
+    def generate_asteroid(self):
+        asteroid = Asteroid.generate([], 0.0)
+        angle = random.random() * 2.0 * math.pi
         dist = random.random() * 10.0 + 10.0
         asteroid.pos = dist * numpy.array([math.cos(angle), math.sin(angle)])
         if asteroid.pos.any():
@@ -179,71 +203,15 @@ class Game(object):
         asteroid.rotation_speed = (-1.0 + 2.0 * random.random()) * 1.0
         return asteroid
 
-    def update(self, delta_time):
-        self.bodies.update(delta_time)
-        pygame.sprite.groupcollide(self.shots, self.asteroids, True, True)
-        if pygame.sprite.spritecollideany(self.player_ship, self.asteroids):
-            sys.exit()
-
-    def draw(self):
-        for body in self.bodies:
-            body.draw()
-
-    @property
-    def time(self):
-        return pygame.time.get_ticks() / 1000.0
-
-def init_display():
-    pygame.display.init()
-    resolution = pygame.display.list_modes()[0]
-    display_surface = pygame.display.set_mode(resolution, FULLSCREEN |
-                                              DOUBLEBUF | OPENGL |
-                                              OPENGLBLIT)
-    glClearColor(0.0, 0.0, 0.0, 0.0)
-    return display_surface
-
-def apply_player_ship_constraints(player_ship, game):
-    velocity_mag = numpy.linalg.norm(player_ship.velocity)
-    if velocity_mag > player_ship.max_velocity:
-        player_ship.velocity *= player_ship.max_velocity / velocity_mag
+    def apply_player_ship_constraints(self):
+        velocity_mag = numpy.linalg.norm(self.player_ship.velocity)
+        if velocity_mag > self.player_ship.max_velocity:
+            self.player_ship.velocity *= (self.player_ship.max_velocity /
+                                          velocity_mag)
 
 def main():
-    pygame.init()
-    pygame.mouse.set_visible(False)
-    init_display()
-    game = Game()
-    quit = False
-    old_time = pygame.time.get_ticks()
-    time_step = 20
-    scale = 50.0
-    while not quit:
-        for event in pygame.event.get():
-            if (event.type == pygame.QUIT or
-                event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
-                quit = True
-        while old_time + time_step < pygame.time.get_ticks():
-            pressed = pygame.key.get_pressed()
-            game.player_ship.thrusting = pressed[pygame.K_UP]
-            if pressed[pygame.K_LEFT]:
-                game.player_ship.rotation_speed = \
-                    game.player_ship.max_rotation_speed
-            elif pressed[pygame.K_RIGHT]:
-                game.player_ship.rotation_speed = \
-                    -game.player_ship.max_rotation_speed
-            else:
-                game.player_ship.rotation_speed = 0.0
-            game.player_ship.firing = pressed[pygame.K_SPACE]
-            game.update(time_step / 1000.0)
-            apply_player_ship_constraints(game.player_ship, game)
-            old_time += time_step
-        glClear(GL_COLOR_BUFFER_BIT)
-        glPushMatrix()
-        x, y = game.player_ship.pos
-        glScaled(0.1, 0.15, 0.1)
-        glTranslated(-x, -y, 0.0)
-        game.draw()
-        glPopMatrix()
-        pygame.display.flip()
+    window = VoidWindow()
+    pyglet.app.run()
     
 if __name__ == '__main__':
     main()
