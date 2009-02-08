e68aa8f16790a1ff24559bc7fbb5e5aed3ca5a60
Mikael Lind <elemel@elemel.se>
2009-02-08 10:09:06 +0100

    removed obsolete cooldown attributes

--- lib/void/ship.py
+++ lib/void/ship.py
@@ -33,8 +33,6 @@ class Ship(Agent):
         self.thrust = False
         self.firing = False
         self.turn = 0.0
-        self.max_cooldown = 0.2
-        self.cooldown = 0.0
         self.max_angular_velocity = 2.0 * math.pi
         self.max_towing_range = 15.0
         self.max_lifeline_range = 200.0
